4b019246bfb92462d3b3be7b35f405812b339396
Roman Gafiyatullin <r.gafiyatullin@me.com>
2021-05-13 16:09:42 +0300

    Remove unused entity

--- src/pg.rs
+++ src/pg.rs
@@ -2,17 +2,3 @@ pub use ::tokio_postgres::types::FromSql as PgFromSql;
 pub use ::tokio_postgres::types::Type as PgType;
 pub use ::tokio_postgres::Column as PgCol;
 pub use ::tokio_postgres::Row as PgRow;
-
-pub struct UnitPgType;
-impl<'a> PgFromSql<'a> for UnitPgType {
-    fn from_sql(
-        _ty: &PgType,
-        _raw: &'a [u8],
-    ) -> Result<Self, Box<dyn std::error::Error + 'static + Send + Sync>> {
-        Ok(Self)
-    }
-
-    fn accepts(_ty: &tokio_postgres::types::Type) -> bool {
-        true
-    }
-}
